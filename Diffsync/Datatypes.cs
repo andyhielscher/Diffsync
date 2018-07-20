616b3bb0da4f6dfc9b2a26b232d4d4a86a2c897d
Andreas Hielscher <andreas.hielscher@smartsim.energy>
2018-07-20 17:21:08 +0200

    add function to get all files in a directory (includes sub directories)

--- Diffsync/Datatypes.cs
+++ Diffsync/Datatypes.cs
@@ -1,16 +1,99 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Datatypes
 {
-    public class ListElement
+    public class FileElement
     {
         string Path;
         double Size;
         DateTime DateCreated;
-        DateTime DateChanged;
+        DateTime DateAccessed;
+        DateTime DateWritten;
 
-        public ListElement()
+        public FileElement()
         {
+        }
+
+        public FileElement(string path)
+        {
+            this.Path = path;
+            FileInfo file_info = new FileInfo(path);
+            this.Size = file_info.Length;
+            this.DateAccessed = file_info.LastAccessTime;
+            this.DateCreated = file_info.CreationTime;
+            this.DateWritten = file_info.LastWriteTime;
+        }
+    }
+
+    public class Parameter
+    {
+        public string main_path;
+        public List<string> directory_exceptions;
+        public DateTime begin_sync_from;
+
+        List<FileElement> all_files = new List<FileElement>();
+        List<FileElement> sync_files = new List<FileElement>();
+
+        public Parameter()
+        {
+            directory_exceptions = new List<string>();
+        }
+
+        public void GetAllFiles()
+        {
+            if (Directory.Exists(main_path) == false)
+            {
+                Console.Error.WriteLine("Hauptverzeichnis {0} nicht gefunden!", main_path);
+                return;
+            }
+
+            GetSubDirectoryFiles(main_path);
+        }
+
+        void GetSubDirectoryFiles(string directory)
+        {
+            string[] sub_directories = Directory.GetDirectories(directory);
+            SortStringAscending(sub_directories);
+            foreach (string sub_directory in sub_directories)
+            {
+                if (IsDirectoryUsable(sub_directory))
+                {
+                    GetSubDirectoryFiles(sub_directory);
+                    GetDirectoryFiles(sub_directory);
+                }
+            }
+        }
+
+        void GetDirectoryFiles(string directory)
+        {
+            string[] files = Directory.GetFiles(directory);
+            SortStringAscending(files);
+            foreach (string file in files)
+            {
+                FileElement file_element = new FileElement(file);
+                all_files.Add(file_element);
+            }
+        }
+
+        void SortStringAscending(string[] strings_to_be_sorted)
+        {
+            Array.Sort(strings_to_be_sorted, (x, y) => String.Compare(x, y));
+        }
+
+        bool IsDirectoryUsable(string directory)
+        {
+            // To-Do: Falls eine Verzeichnis-Ausnahme aus mehr Ebenen besteht, muss der Code angepasst werden
+            string[] single_folders = directory.Split('\\');
+            foreach (string directory_exception in directory_exceptions)
+            {
+                if (single_folders[single_folders.Length - 1].ToLower() == directory_exception.TrimEnd('\\').ToLower())
+                {
+                    return (false);
+                }
+            }
+            return (true);
         }
     }
 }
--- Diffsync/Program.cs
+++ Diffsync/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Datatypes; 
+using Datatypes;
 
 namespace Diffsync
 {
@@ -11,6 +11,17 @@ namespace Diffsync
     {
         static void Main(string[] args)
         {
+            Parameter parameter = new Parameter();
+
+            // Arbeitsvariablen (aktuell hardgecodet):
+            parameter.main_path = @"C:\Users\AndreasHielscher\OneDrive - SmartSim GmbH\Dokumente\";
+            parameter.directory_exceptions = new List<string>();
+            parameter.directory_exceptions.Add("Benutzerdefinierte Office-Vorlagen");
+            parameter.directory_exceptions.Add("EON");
+            parameter.begin_sync_from = new DateTime(2018, 7, 10, 0, 0, 0);
+
+            // Verzeichnis einlesen
+            parameter.GetAllFiles();
         }
     }
 }
