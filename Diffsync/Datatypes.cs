6586ade038cc042bb93994f2a39d7e6cb6dbd511
Andreas Hielscher <andreas.hielscher@smartsim.energy>
2018-07-23 15:00:22 +0200

    add function to get all files which will be copied, print them to console and wait for user input

--- Diffsync/Datatypes.cs
+++ Diffsync/Datatypes.cs
@@ -6,11 +6,72 @@ namespace Datatypes
 {
     public class FileElement
     {
-        string Path;
-        double Size;
-        DateTime DateCreated;
-        DateTime DateAccessed;
-        DateTime DateWritten;
+        public string Path {
+            get
+            {
+                const int num_letters = 80;
+                if (this._path.Length < num_letters)
+                {
+                    return (String.Format("{0,-" + num_letters + "}", this._path));
+                } else
+                {
+                    string[] parts = this._path.Split('\\');
+                    string path_shortend = "";
+                    int path_shortend_length;
+
+                    path_shortend_length = parts[parts.Length - 1].Length;
+                    if (path_shortend_length < num_letters)
+                    {
+                        int i = 0;
+                        do
+                        {
+                            if (parts[i].Length < num_letters - path_shortend_length - 4)
+                            {
+                                path_shortend = String.Format("{0}{1}\\", path_shortend, parts[i]);
+                                path_shortend_length += parts[i].Length + 1;
+                            }
+                            else
+                            {
+                                path_shortend = String.Format("{0}{1}...\\", path_shortend, parts[i].Substring(0, num_letters - path_shortend_length - 4));
+                                break;
+                            }
+                            i++;
+                        } while (i < parts.Length-1);
+                        path_shortend = String.Format("{0}{1}", path_shortend, parts[parts.Length - 1]);
+                    } else
+                    {
+                        path_shortend = String.Format("{0}...{1}", parts[parts.Length - 1].Substring(0, num_letters / 2 - 2), parts[parts.Length - 1].Substring(path_shortend_length - num_letters / 2 + 1));
+                    }
+                    return (path_shortend);
+                }
+            }
+        }
+        public double Size {
+            get
+            {
+                return (this._size);
+            }
+        }
+        public DateTime Date_created {
+            get
+            {
+                return (this._date_created);
+            }
+        }
+        /* This method may return an inaccurate value, because it uses native functions whose values may not be continuously updated by the operating system.
+         * https://msdn.microsoft.com/en-us/library/system.io.filesysteminfo.lastaccesstime(v=vs.110).aspx */
+        //DateTime DateAccessed; 
+        public DateTime Date_written {
+            get
+            {
+                return (this._date_written);
+            }
+        }
+
+        string _path;
+        double _size;
+        DateTime _date_created;
+        DateTime _date_written;
 
         public FileElement()
         {
@@ -18,44 +79,58 @@ namespace Datatypes
 
         public FileElement(string path)
         {
-            this.Path = path;
+            this._path = path;
             FileInfo file_info = new FileInfo(path);
-            this.Size = file_info.Length;
-            this.DateAccessed = file_info.LastAccessTime;
-            this.DateCreated = file_info.CreationTime;
-            this.DateWritten = file_info.LastWriteTime;
+            this._size = file_info.Length;
+            //this.DateAccessed = file_info.LastAccessTime;
+            this._date_created = file_info.CreationTime;
+            this._date_written = file_info.LastWriteTime;
+        }
+
+        public bool IsNewOrChanged(DateTime new_datetime)
+        {
+            if (this.Date_created > new_datetime || this.Date_written > new_datetime)
+            {
+                return (true);
+            } else
+            {
+                return (false);
+            }
         }
     }
 
     public class Parameter
     {
-        public string main_path;
-        public List<string> directory_exceptions;
-        public DateTime begin_sync_from;
+        public string Main_path { get; set; }
+        public List<string> Directory_exceptions { get; set; }
+        public DateTime Begin_sync_from { get; set; }
+        public string Project_name { get; set; }
 
-        List<FileElement> all_files = new List<FileElement>();
-        List<FileElement> sync_files = new List<FileElement>();
+        List<FileElement> _all_files = new List<FileElement>();
+        List<FileElement> _sync_files = new List<FileElement>();
 
         public Parameter()
         {
-            directory_exceptions = new List<string>();
+            Directory_exceptions = new List<string>();
         }
 
         public void GetAllFiles()
         {
-            if (Directory.Exists(main_path) == false)
+            if (Directory.Exists(Main_path) == false)
             {
-                Console.Error.WriteLine("Hauptverzeichnis {0} nicht gefunden!", main_path);
+                Console.Error.WriteLine("Hauptverzeichnis {0} nicht gefunden!", Main_path);
                 return;
             }
 
-            GetSubDirectoryFiles(main_path);
+            GetSubDirectoryFiles(Main_path);
         }
 
         void GetSubDirectoryFiles(string directory)
         {
             string[] sub_directories = Directory.GetDirectories(directory);
+
             SortStringAscending(sub_directories);
+
             foreach (string sub_directory in sub_directories)
             {
                 if (IsDirectoryUsable(sub_directory))
@@ -69,11 +144,13 @@ namespace Datatypes
         void GetDirectoryFiles(string directory)
         {
             string[] files = Directory.GetFiles(directory);
+
             SortStringAscending(files);
+
             foreach (string file in files)
             {
                 FileElement file_element = new FileElement(file);
-                all_files.Add(file_element);
+                _all_files.Add(file_element);
             }
         }
 
@@ -86,7 +163,8 @@ namespace Datatypes
         {
             // To-Do: Falls eine Verzeichnis-Ausnahme aus mehr Ebenen besteht, muss der Code angepasst werden
             string[] single_folders = directory.Split('\\');
-            foreach (string directory_exception in directory_exceptions)
+
+            foreach (string directory_exception in Directory_exceptions)
             {
                 if (single_folders[single_folders.Length - 1].ToLower() == directory_exception.TrimEnd('\\').ToLower())
                 {
@@ -94,6 +172,21 @@ namespace Datatypes
                 }
             }
             return (true);
+        }
+
+        public List<FileElement> GetFilesToCopy()
+        {
+            List<FileElement> files_to_copy = new List<FileElement>();
+
+            foreach (FileElement file_element in _all_files)
+            {
+                if (file_element.IsNewOrChanged(Begin_sync_from))
+                {
+                    files_to_copy.Add(file_element);
+                }
+            }
+
+            return (files_to_copy);
         }
     }
 }
--- Diffsync/Program.cs
+++ Diffsync/Program.cs
@@ -13,15 +13,71 @@ namespace Diffsync
         {
             Parameter parameter = new Parameter();
 
+            // Console: Breite einstellen
+            Console.WindowWidth = 130;
+
             // Arbeitsvariablen (aktuell hardgecodet):
-            parameter.main_path = @"C:\Users\AndreasHielscher\OneDrive - SmartSim GmbH\Dokumente\";
-            parameter.directory_exceptions = new List<string>();
-            parameter.directory_exceptions.Add("Benutzerdefinierte Office-Vorlagen");
-            parameter.directory_exceptions.Add("EON");
-            parameter.begin_sync_from = new DateTime(2018, 7, 10, 0, 0, 0);
+            parameter.Main_path = @"C:\Users\AndreasHielscher\OneDrive - SmartSim GmbH\Dokumente\";
+            parameter.Directory_exceptions = new List<string>();
+            parameter.Directory_exceptions.Add("Benutzerdefinierte Office-Vorlagen");
+            parameter.Directory_exceptions.Add("EON");
+            parameter.Begin_sync_from = new DateTime(2018, 7, 10, 0, 0, 0);
+            parameter.Project_name = "Sync Andis PC";
 
             // Verzeichnis einlesen
             parameter.GetAllFiles();
+
+            // Dateien auflisten, welche kopiert werden sollen
+            List<FileElement> files_to_copy = parameter.GetFilesToCopy();
+
+            // Dateien ausgeben, welche kopiert werden
+            PrintFilesToCopy(ref files_to_copy);
+
+            // Auf Bestätigung des Users zum weiteren Programmablauf warten
+            if (UserWantsToContinue() == false)
+            {
+                Environment.Exit(0);
+            }
         }
-    }
+
+        static void PrintFilesToCopy(ref List<FileElement> files)
+        {
+            Console.WriteLine("Es folgt die Ausgabe aller Dateien, die kopiert werden:");
+            foreach (FileElement file_element in files)
+            {
+                Console.WriteLine("{0}  {1}  {2}  {3:0.000} MB",
+                    file_element.Path,
+                    file_element.Date_created.ToString("yyyy-MM-dd HH:mm"),
+                    file_element.Date_written.ToString("yyyy-MM-dd HH:mm"),
+                    file_element.Size / 1024 / 1024);
+            }
+        }
+
+        static bool UserWantsToContinue()
+        {
+            bool false_input;
+            string input;
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("Soll mit dem Kopieren begonnen werden (j/n)?");
+                input = Console.ReadLine();
+                if (input == "j" || input == "n")
+                {
+                    false_input = false;
+                }
+                else
+                {
+                    false_input = true;
+                }
+            } while (false_input);
+            if (input == "j")
+            {
+                return (true);
+            } else
+            {
+                return (false);
+            }
+        }
+    } 
 }
